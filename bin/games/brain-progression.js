7c0495345315b6ba0b6801a2e7df5cd226acb6aa
Stanislav Stolyarov <stst09stanislav@yandex.ru>
2025-11-23 00:11:04 +0700

    Шаг 8 - Четвётрая игра

---
+++ bin/games/brain-progression.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import {brainGames} from "../brain-games.js";
+
+const name = brainGames();
+
+console.log('What number is missing in the progression?');
+
+
+for(let i = 0; i < 3; i++){
+    
+    var amount = Math.floor(Math.random() * 11);
+    if (amount < 5){
+        amount = 5;
+    }
+    var unvisible = Math.floor(Math.random() * (amount + 1));
+    var step = Math.floor(Math.random() * 10);
+    var nums = "";
+
+    var start = Math.floor(Math.random() * 100);
+    for(let i = 0; i <= amount; i++){
+        if (i == unvisible){
+            nums += `.. `;
+            var trueAnswer = start;
+        }else{
+            nums += `${start} `;
+        }
+        start = start + step;
+    }
+
+
+    console.log(`Question: ${nums}`);
+
+    const answer = readlineSync.question('Your answer: ');
+    if (answer == trueAnswer){
+            console.log("Correct!");
+            if (i == 2){
+                console.log(`Congratulations, ${name}!`);
+            }
+        }else{
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`);
+            console.log(`Let's try again, ${name}!`);
+            break;
+    }
+
+}
