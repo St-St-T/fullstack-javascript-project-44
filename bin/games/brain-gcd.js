eb43a1b7ab6ee02bea022154876122708df89fc4
Stanislav Stolyarov <stst09stanislav@yandex.ru>
2025-11-23 00:09:19 +0700

    Шаг 7 - Третья игра

---
+++ bin/games/brain-gcd.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import {brainGames} from "../brain-games.js";
+
+const name = brainGames();
+
+console.log('Find the greatest common divisor of given numbers.');
+
+for(let i = 0; i < 3; i++){
+    
+    var num1 = Math.floor(Math.random() * 100);
+    var num2 = Math.floor(Math.random() * 100);
+    var result;
+
+    function gcd (x, y) {
+        if (y > x) return gcd(y, x);
+        if (!y) return x;
+        return gcd(y, x % y);
+    }
+    
+    var trueAnswer = gcd(num1, num2);
+    console.log(`Question: ${num1} ${num2}`);
+
+    const answer = readlineSync.question('Your answer: ');
+    
+    if (answer == trueAnswer){
+            console.log("Correct!");
+            if (i == 2){
+                console.log(`Congratulations, ${name}!`);
+            }
+        }else{
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`);
+            console.log(`Let's try again, ${name}!`);
+            break;
+    }
+
+}
